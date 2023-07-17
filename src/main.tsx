45cecc5a4160a7741e6ea1367e6bcd62285e5da1
GeeksforGeeks <prajyothere@gmail.com>
2023-07-17 10:59:26 +0530

    Original Files uploaded

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 // import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import MyHomePage from './pages/HomePage.tsx'
-import BlogSectionPage from './pages/BlogSectionPage/BlogSectionPage.tsx'
-import BlogPage from './pages/BlogPage/BlogPage.tsx'
-import ProjectSectionPage from './pages/ProjectSectionPage/ProjectSectionPage.tsx'
-import ProjectPage from './pages/ProjectPage/ProjectPage.tsx'
-import AboutPage from './pages/AboutPage/AboutPage.tsx'
+// import MyHomePage from './pages/HomePage.tsx'
+// import BlogSectionPage from './pages/BlogSectionPage/BlogSectionPage.tsx'
+// import BlogPage from './pages/BlogPage/BlogPage.tsx'
+// import ProjectSectionPage from './pages/ProjectSectionPage/ProjectSectionPage.tsx'
+// import ProjectPage from './pages/ProjectPage/ProjectPage.tsx'
+// import AboutPage from './pages/AboutPage/AboutPage.tsx'
 
 // const router= createBrowserRouter([
 //   {
